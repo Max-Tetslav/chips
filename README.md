5a2daeef59c55a2223bf269329c66563c3ee5761
Maksim Tetslav <maks.tetslav@yandex.ru>
2025-08-03 19:32:19 +0300

    init: create project

---
+++ README.md
@@ -0,0 +1,30 @@
+# 🍟 Тестовые чипсы
+
+## 🌐 Деплой
+
+[Live Demo на GitHub Pages](https://max-tetslav.github.io/chips/)
+
+## 📦 Stack
+
+React, TypeScript, Vite, Vitest, Testing-Library, Eslint, Prettier, GitHub Actions CI/CD
+
+## 🚀 Запуск проекта
+
+```bash
+npm i && npm run start
+```
+
+## 🧪 Запуск тестов
+
+```bash
+npm run test
+```
+
+## 🖼️ Описание приложения
+
+### Скриншот интерфейса:
+
+### Функционал
+- 
+
+
---
+++ eslint.config.js
@@ -0,0 +1,76 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
+
+export default tseslint.config(
+    { ignores: ['dist', 'coverage'] },
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
+        files: ['src/**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                project: './tsconfig.json'
+            }
+        },
+
+        settings: {
+            'import/parsers': {
+                '@typescript-eslint/parser': ['.ts', '.tsx']
+            },
+            'import/resolver': {
+                typescript: {
+                    alwaysTryTypes: true,
+                    project: './tsconfig.json'
+                }
+            }
+        },
+
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            'unused-imports': eslintPluginUnusedImports,
+            import: eslintPluginImport,
+            prettier: eslintPluginPrettier
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': ['error', { allowConstantExport: true }],
+            'prettier/prettier': 'error',
+            'unused-imports/no-unused-imports': 'error',
+            'unused-imports/no-unused-vars': [
+                'error',
+                {
+                    vars: 'all',
+                    varsIgnorePattern: '^_',
+                    args: 'after-used',
+                    argsIgnorePattern: '^_'
+                }
+            ],
+            'import/order': [
+                'error',
+                {
+                    groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+                    pathGroups: [
+                        {
+                            pattern: '@(app|pages|widgets|features|entities|shared)/**',
+                            group: 'internal',
+                            position: 'after'
+                        }
+                    ],
+                    pathGroupsExcludedImportTypes: ['builtin', 'external'],
+                    'newlines-between': 'always',
+                    alphabetize: { order: 'asc', caseInsensitive: false }
+                }
+            ],
+            'import/no-duplicates': 'error'
+        }
+    }
+);
